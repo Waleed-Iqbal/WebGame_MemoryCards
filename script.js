ee709e715b3b82af9e109585a08041f3b33ae4fe
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2018-08-19 00:07:08 +0500

    Game functioning

---
+++ script.js
@@ -0,0 +1,62 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+
+function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add("flip");
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    hasFlippedCard = false;
+    secondCard = this;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    firstCard.dataset.cardname === secondCard.dataset.cardname ?
+        disableCards() :
+        unFlipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unFlipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        lockBoard = false;
+    }, 1500);
+}
+
+function resetBoard() {
+    hasFlippedCard = false;
+    lockBoard= false;
+    firstCard = null;
+    secondCard = null;
+}
+
+(function shuffle(){
+    cards.forEach(card => {
+        let randomPos = Math.floor((Math.random() * 12));
+        card.style.order = randomPos;
+    })
+}())
+
+
+
+cards.forEach(card => card.addEventListener("click", flipCard));
